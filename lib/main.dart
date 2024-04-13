6de5cdba1c6f1cd3f0fe271d030321d99d0de96c
penne <penne0505pp@gmail.com>
2024-04-14 00:07:56 +0900

    デザイン、レイアウトの修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:penkiel/components/sidebar.dart';
+import 'package:penkiel/components/topnav.dart';
+import 'package:penkiel/utils.dart';
+
+
 void main() {
   runApp(const PenkielApp());
 }
@@ -11,11 +16,18 @@ class PenkielApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      home: const MyHomePage(title: 'Penkiel App'),
+      scrollBehavior: CustomScrollBehavior(),
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFF2Bf5E), background: const Color(0xFFFDFDFD)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFF2Bf5E), background: const Color(0xFFFDFDFD), surfaceTint: const Color(0x00ffffff)),
+        scrollbarTheme: ScrollbarThemeData(
+          radius: const Radius.circular(10),
+          thickness: MaterialStateProperty.all(4),
+          thumbColor: MaterialStateProperty.all(const Color(0xFFD8D8D8)),
+        ),
+        fontFamilyFallback: const <String>['Inter', 'Inter Display'],
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Penkiel App'),
     );
   }
 }
@@ -30,40 +42,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'ボタンをこれだけ押した回数:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'インクリメント',
-        child: const Icon(Icons.add),
-      ),
+      appBar: const TopNav(),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if ((600 < constraints.maxWidth) && (constraints.maxWidth < 1200)) {
+            return const Row(
+              children: <Widget>[
+                // ! 実行して、ウィンドウの横幅を速く動かすと、エラーが出る。これ直す！！
+                Expanded(
+                  flex: 1,
+                  child: SideBar(),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Center(
+                    child: Text('Main Content'),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const Center(
+              child: Text('Main Content'),
+            );
+          }
+        }
+      )
     );
   }
 }
