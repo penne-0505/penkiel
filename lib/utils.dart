90ac28ce8628ebe4becd9d9b24b1bf108c756c3f
penne <penne0505pp@gmail.com>
2024-04-14 00:08:17 +0900

    スクロールの対応バイスの変更

---
+++ lib/utils.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.trackpad,
+      };
+}
