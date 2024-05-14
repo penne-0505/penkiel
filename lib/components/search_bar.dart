be4d5c4ce84c562f063bac0fb00ba974840840a2
penne <penne0505pp@gmail.com>
2024-05-15 07:04:37 +0900

    feat: SearchBarの影が、フォーカスされている時にのみ表示される機能を追加
    SearchBarの影が、tabでのフォーカス、クリック、タップでのフォーカスが行われている時のみ影が表示されるようになりました
    
    特になし

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:penkiel/utils.dart';
 
 class SearchField extends StatefulWidget {
@@ -10,22 +9,48 @@ class SearchField extends StatefulWidget {
 }
 
 class _SearchFieldState extends State<SearchField> {
+  final FocusNode _focusNode = FocusNode();
+  bool _isFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(_updateState);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_updateState);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _updateState() {
+    setState(() {
+      _isFocused = _focusNode.hasFocus;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         color: PenkielColors.bg,
-        borderRadius: BorderRadius.all(Radius.circular(18)),
-        boxShadow: [
-          BoxShadow(
-            color: PenkielColors.shadow,
-            blurRadius: 2,
-            offset: Offset(1, 2),
-          ),
-        ],
+        borderRadius: BorderRadius.circular(18),
+        boxShadow: _isFocused
+            ? [
+                const BoxShadow(
+                  color: PenkielColors.shadow,
+                  blurRadius: 2,
+                  offset: Offset(0.5, 2),
+                ),
+              ]
+            : null,
       ),
-      child: const TextField(
-        decoration: InputDecoration(
+      child: TextField(
+        focusNode: _focusNode,
+        decoration: const InputDecoration(
           fillColor: PenkielColors.bg,
           border: InputBorder.none,
           prefixIcon: Padding(
