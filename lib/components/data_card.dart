c3bc4e9968974a6e7d0f0dd4eedc160452a037d0
penne <penne0505pp@gmail.com>
2024-06-13 19:48:41 +0900

    feat: DataCardのデザインを更新(未完成)
    DataCardのデザインを最低限必要な形に修正、更新しました。また、マウスのhover、focusのイベントに対応しました。
    
    破壊的変更はありません。

--- lib/components/data_card.dart
+++ lib/components/data_card.dart
@@ -1,94 +1,71 @@
-import 'package:dotted_border/dotted_border.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 import 'package:penkiel/utils.dart';
 
-class DataCard extends StatelessWidget {
-  const DataCard({
-    super.key,
-    this.cardType = 'normal',
-    this.cardHeight = 80,
-    });
+class DataCard extends StatefulWidget {
+  const DataCard({super.key});
 
-    /// cardType must be one of 'normal', 'multiline', 'picture', 'file', or 'custom_height'.
-    final String cardType;
-    final double? cardHeight;
+  @override
+  State<DataCard> createState() => _DataCardState();
+}
 
-  // !注意: 切り替えを実装しただけ、実際のコンテンツは未実装
+class _DataCardState extends State<DataCard> {
+  bool isHovered = false;
+
   @override
   Widget build(BuildContext context) {
-    final Widget normalDataCard =
-    Container(
-      width: double.infinity,
-      margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 100),
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-      decoration: BoxDecoration(
-        color: PenkielColors.blendedSecff,
-        borderRadius: const BorderRadius.all(PenkielValues.borderRadius),
-        boxShadow: [
-          PenkielShadow().neumorphicBottom,
-          PenkielShadow().neumorphicTop,
-        ],
-      ),
-      child: Row(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 4),
-            child: const Text(
-              'Name',
-              style: TextStyle(
-                color: PenkielColors.mask,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0.8,
-              ),
-            ),
-          ),
-          Container(
-            width: 2,
-            height: 32,
-            margin: const EdgeInsets.symmetric(horizontal: 24),
-            decoration: const BoxDecoration(
-              color: PenkielColors.dividerColor,
-            ),
-          ),
-          Container(
-            width: 300,
-            margin: const EdgeInsets.symmetric(horizontal: 32),
-            child: DottedBorder(
-              color: PenkielColors.dividerColor,
-              strokeWidth: 1,
-              borderType: BorderType.RRect,
-              dashPattern: const [5, 5],
-              radius: const Radius.circular(8),
-              child: const TextField(
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: MouseRegion(
+        onEnter: (event) {
+          setState(() {
+            isHovered = true;
+          });
+        },
+        onExit: (event) {
+          setState(() {
+            isHovered = false;
+          });
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(flex: 1, child: Container(color: Colors.amber,)), // Spacer
+            Expanded(
+              flex: 3,
+              child: TextField(
                 maxLines: 1,
-                minLines: 1,
                 decoration: InputDecoration(
-                  border: InputBorder.none,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: isHovered ? PenkielColors.primary : PenkielColors.dividerColor,
+                      width: 1,
+                    ),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    borderSide: BorderSide(
+                      color: PenkielColors.primary,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                style: const TextStyle(
+                  color: PenkielColors.text,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'Campton',
+                  letterSpacing: 0.05,
                 ),
               ),
             ),
-          ),
-        ],
-      )
+            Expanded(flex: 1, child: Container(color: Colors.amber,)), // Spacer
+          ],
+        ),
+      ),
     );
-  
-
-    // switch (cardType) {
-    //   case 'normal':
-    //     return normalDataCard;
-    //   case 'multiline':
-    //     return multilineDataCard;
-    //   case 'picture':
-    //     return pictureDataCard;
-    //   case 'custom_height':
-    //     return customHeightDataCard;
-    //   case 'file':
-    //     return fileDataCard;
-    //   default:
-    //     return normalDataCard;
-    // }
-    return normalDataCard;
   }
 }
