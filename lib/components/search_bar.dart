c558f2e5cdb11ccdcffba2640678c3387e3f4085
penne <penne0505pp@gmail.com>
2024-06-13 19:50:58 +0900

    feat: hoverの状態によってborderの色が変化するようにしました
    hoverの状態監視の処理を追加し、TextFieldのBorderの色が変化するようにしました。
    
    破壊的変更はありません。

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:penkiel/utils.dart';
 
 class SearchField extends StatefulWidget {
-  const SearchField({super.key});
+  const SearchField({Key? key}) : super(key: key);
 
   @override
   State<SearchField> createState() => _SearchFieldState();
@@ -11,6 +11,7 @@ class SearchField extends StatefulWidget {
 class _SearchFieldState extends State<SearchField> {
   final FocusNode _focusNode = FocusNode();
   bool _isFocused = false;
+  bool _isHovered = false;
 
   @override
   void initState() {
@@ -31,26 +32,37 @@ class _SearchFieldState extends State<SearchField> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: PenkielColors.bg,
-        borderRadius: BorderRadius.circular(18),
-        border: Border.all(
-          color: _isFocused ? PenkielColors.v400 : PenkielColors.dividerColor,
-          width: 1,
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() {
+          _isHovered = true;
+        });
+      },
+      onExit: (_) {
+        setState(() {
+          _isHovered = false;
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: PenkielColors.bg,
+          borderRadius: BorderRadius.circular(18),
+          border: Border.all(
+            color: _isFocused || _isHovered ? PenkielColors.v400 : PenkielColors.dividerColor,
+            width: 1,
+          ),
         ),
-      ),
-      child: TextField(
-        focusNode: _focusNode,
-        decoration: const InputDecoration(
-          fillColor: PenkielColors.bg,
-          border: InputBorder.none,
-          prefixIcon: Padding(
-            padding: EdgeInsets.only(left: 12),
-            child: Icon(Icons.search),
+        child: TextField(
+          focusNode: _focusNode,
+          decoration: const InputDecoration(
+            fillColor: PenkielColors.bg,
+            border: InputBorder.none,
+            prefixIcon: Padding(
+              padding: EdgeInsets.only(left: 12),
+              child: Icon(Icons.search),
+            ),
           ),
         ),
       ),
