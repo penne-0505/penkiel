928ed4d98e8ac501901688dbd3712e2305e9914f
penne <penne0505pp@gmail.com>
2024-06-15 11:06:53 +0900

    feat: data_cardにおけるデザインを更新
    破壊的変更はありません。

--- lib/components/data_card.dart
+++ lib/components/data_card.dart
@@ -16,55 +16,63 @@ class _DataCardState extends State<DataCard> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 12),
-      child: MouseRegion(
-        onEnter: (event) {
-          setState(() {
-            isHovered = true;
-          });
-        },
-        onExit: (event) {
-          setState(() {
-            isHovered = false;
-          });
-        },
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(flex: 1, child: Container(color: Colors.amber,)), // Spacer
-            Expanded(
-              flex: 3,
-              child: TextField(
-                maxLines: 1,
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(
-                      color: isHovered ? PenkielColors.primary : PenkielColors.dividerColor,
-                      width: 1,
-                    ),
-                  ),
-                  focusedBorder: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                    borderSide: BorderSide(
-                      color: PenkielColors.primary,
-                      width: 2,
-                    ),
-                  ),
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(flex: 5, child: Container()), // Spacer
+          Expanded(
+            flex: 12,
+            child: MouseRegion(
+              onEnter: (_) => setState(() => isHovered = true),
+              onExit: (_) => setState(() => isHovered = false),
+              child: Container(
+                margin: const EdgeInsets.all(1),
+                decoration: BoxDecoration(
+                  color: PenkielColors.bg,
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                style: const TextStyle(
-                  color: PenkielColors.text,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  fontFamily: 'Campton',
-                  letterSpacing: 0.05,
+                child: TextField(
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    labelText: 'Data',
+                    fillColor: PenkielColors.bg,
+                    labelStyle: TextStyle(
+                      color: isHovered ? PenkielColors.highContrastText : PenkielColors.disabledText,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'Campton',
+                      letterSpacing: 0.05,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(
+                        color: isHovered ? PenkielColors.v400 : PenkielColors.dividerColor,
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(
+                        color: PenkielColors.v400,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  style: const TextStyle(
+                    color: PenkielColors.text,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Campton',
+                    letterSpacing: 0.05,
+                  ),
                 ),
               ),
             ),
-            Expanded(flex: 1, child: Container(color: Colors.amber,)), // Spacer
-          ],
-        ),
+          ),
+          Expanded(flex: 5, child: Container()), // Spacer
+        ],
       ),
     );
   }
