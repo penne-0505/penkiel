4ce398b634eef38ed39ffc24f8397a9bf93520b1
penne <penne0505pp@gmail.com>
2024-06-15 11:07:52 +0900

    refactor: search_barにおけるhover,focus検出処理の簡易化
    破壊的変更はありません。

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -2,67 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:penkiel/utils.dart';
 
 class SearchField extends StatefulWidget {
-  const SearchField({Key? key}) : super(key: key);
+  const SearchField({super.key});
 
   @override
   State<SearchField> createState() => _SearchFieldState();
 }
 
 class _SearchFieldState extends State<SearchField> {
-  final FocusNode _focusNode = FocusNode();
-  bool _isFocused = false;
-  bool _isHovered = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _focusNode.addListener(_updateState);
-  }
-
-  @override
-  void dispose() {
-    _focusNode.removeListener(_updateState);
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  void _updateState() {
-    setState(() {
-      _isFocused = _focusNode.hasFocus;
-    });
-  }
+  bool isHovered = false;
 
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
-      onEnter: (_) {
-        setState(() {
-          _isHovered = true;
-        });
-      },
-      onExit: (_) {
-        setState(() {
-          _isHovered = false;
-        });
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          color: PenkielColors.bg,
-          borderRadius: BorderRadius.circular(18),
-          border: Border.all(
-            color: _isFocused || _isHovered ? PenkielColors.v400 : PenkielColors.dividerColor,
-            width: 1,
+      onEnter: (_) => setState(() => isHovered = true),
+      onExit: (_) => setState(() => isHovered = false),
+      child: TextField(
+        decoration: InputDecoration(
+          fillColor: PenkielColors.bg,
+
+          enabledBorder: OutlineInputBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(16)),
+            borderSide: BorderSide(
+              color: isHovered ? PenkielColors.v400 : PenkielColors.dividerColor,
+              width: 1,
+            ),  
           ),
-        ),
-        child: TextField(
-          focusNode: _focusNode,
-          decoration: const InputDecoration(
-            fillColor: PenkielColors.bg,
-            border: InputBorder.none,
-            prefixIcon: Padding(
-              padding: EdgeInsets.only(left: 12),
-              child: Icon(Icons.search),
+          focusedBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+            borderSide: BorderSide(
+              color: PenkielColors.dividerColor,
+              width: 1,
             ),
+          ),
+          prefixIcon: const Padding(
+            padding: EdgeInsets.only(left: 12),
+            child: Icon(Icons.search),
           ),
         ),
       ),
