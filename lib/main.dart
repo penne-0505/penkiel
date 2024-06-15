2f40d364df4110bc47eb88c0cf9d61b2a367ff92
penne <penne0505pp@gmail.com>
2024-06-15 10:02:36 +0900

    refactor: ページの定義を別ファイルに移動しました
    main.dartで定義していたメインページを、ページ定義専用ファイルに移動しました。
    
    ファイルを直接参照していたプログラムが存在する場合、参照ができなくなりました。

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:penkiel/components/layout/data_list.dart';
-import 'package:penkiel/components/layout/sidebar.dart';
-import 'package:penkiel/components/layout/topnav.dart';
-import 'package:penkiel/components/layout/data_content.dart';
+import 'package:penkiel/pages/main_content.dart';
 import 'package:penkiel/utils.dart';
 
 
@@ -18,7 +15,7 @@ class PenkielApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Penkiel',
-      home: const MyHomePage(title: 'Penkiel'),
+      home: const MainContentPage(title: 'Penkiel'),
       scrollBehavior: CustomScrollBehavior(),
       theme: ThemeData(
         fontFamilyFallback: const <String>['Inter', 'Roboto', 'Arial', 'sans-serif'],
@@ -32,51 +29,6 @@ class PenkielApp extends StatelessWidget {
           background: PenkielColors.bg,
           ),
       )
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key, 
-    required this.title,
-  });
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const TopNav(),
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          if (constraints.maxWidth > 1535) {
-            return Row(
-              children: <Widget>[
-                const Expanded(
-                  flex: 12,
-                  child: SideBar(),
-                ),
-                Expanded(
-                  flex: 18,
-                  child: DataList(),
-                ),
-                const Expanded(
-                  flex: 40,
-                  child: DataContent()
-                ),
-              ],
-            );
-          } else {
-            return const DataContent();
-          }
-        },
-      ),
     );
   }
 }
---
+++ lib/pages/main_content.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:penkiel/components/layout/data_content.dart';
+import 'package:penkiel/components/layout/data_list.dart';
+import 'package:penkiel/components/layout/sidebar.dart';
+import 'package:penkiel/components/layout/topnav.dart';
+
+class MainContentPage extends StatefulWidget {
+  const MainContentPage({
+    super.key, 
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<MainContentPage> createState() => _MainContentPageState();
+}
+
+class _MainContentPageState extends State<MainContentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const TopNav(),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth > 1535) {
+            return Row(
+              children: <Widget>[
+                const Expanded(
+                  flex: 12,
+                  child: SideBar(),
+                ),
+                Expanded(
+                  flex: 18,
+                  child: DataList(),
+                ),
+                const Expanded(
+                  flex: 40,
+                  child: DataContent()
+                ),
+              ],
+            );
+          } else {
+            return const DataContent();
+          }
+        },
+      ),
+    );
+  }
+}
